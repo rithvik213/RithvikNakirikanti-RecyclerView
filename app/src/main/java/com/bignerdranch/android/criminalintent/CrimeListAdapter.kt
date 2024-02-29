a8959904bcb95f9674d261765cfb1839d9aefa70
rithvik <rithvik@rntech.org>
2024-02-28 20:51:29 -0500

    made a lot of modifications and made the new XML for serious crimes and color coded it to purple to highlight the difference.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
+import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeSeriousBinding
 
 class CrimeHolder(
     private val binding: ListItemCrimeBinding
@@ -23,21 +24,60 @@ class CrimeHolder(
     }
 }
 
+class SeriousCrimeHolder(
+    private val binding: ListItemCrimeSeriousBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(crime: Crime) {
+        binding.crimeTitle.text = crime.title
+        binding.crimeDate.text = crime.date.toString()
+
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
+
 class CrimeListAdapter(
     private val crimes: List<Crime>
-) : RecyclerView.Adapter<CrimeHolder>() {
+
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    enum class CrimeType {
+        NORMAL, SERIOUS
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (crimes[position].requiresPolice) CrimeType.SERIOUS.ordinal else CrimeType.NORMAL.ordinal
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): CrimeHolder {
+    ): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
-        return CrimeHolder(binding)
+        return when (viewType) {
+            CrimeType.NORMAL.ordinal -> {
+                val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+                CrimeHolder(binding)
+            }
+            CrimeType.SERIOUS.ordinal -> {
+                val binding = ListItemCrimeSeriousBinding.inflate(inflater, parent, false)
+                SeriousCrimeHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view")
+        }
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val crime = crimes[position]
-        holder.bind(crime)
+        when (holder) {
+            is CrimeHolder -> holder.bind(crime)
+            is SeriousCrimeHolder -> holder.bind(crime)
+        }
     }
 
     override fun getItemCount() = crimes.size
